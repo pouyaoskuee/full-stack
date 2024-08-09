04495de60d65ae7765d524c64b9c6a2fc77663ee
pouya <pouyakhaleghi82@gmail.com>
2024-08-09 04:21:23 +0330

    start

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/materialize/css/materialize.min.css">
+    <link rel="stylesheet" href="assets/stayle.css">
+    <link  rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php include 'layouts/header.php'; ?>
+
+
+
+    <script src="assets/materialize/js/materialize.min.js"></script>
+    <script src="assets/script.js"></script>
+</body>
+</html>
